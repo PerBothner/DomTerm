01339223793f1027f92870923296eb0e95c26408
“Per <per@bothner.com>
2015-11-22 22:58:11 -0800

    Add features and fix various bugs. Major changes: Add popup-menu to ptyconsole.App. Implement mode to automatically switch between line and character modes. Handle window re-sizing in server mode. Change the wrap indicator to be css-only.

---
+++ ptyconsole/Util.java
@@ -0,0 +1,53 @@
+package ptyconsole;
+
+public class Util {
+
+    /** Parse a string formatted using JSON.stringify */
+    public static String parseSimpleJsonString(String str, int start, int end) {
+        StringBuilder buf = new StringBuilder();
+        char ch0 = 0;
+        int i = start;
+        for (;;) {
+            if (i >= end)
+                return null;
+            ch0 = str.charAt(i++);
+            if (ch0 == '"' || ch0 == '\'')
+                break;
+            if (! Character.isWhitespace(ch0))
+                return null;
+        }
+        for (; i < end; ) {
+            char ch = str.charAt(i++);
+            if (ch == ch0)
+                break;
+            if (ch == '\\') {
+                if (i == end)
+                    return null;
+                ch = str.charAt(i++);
+                switch (ch) {
+                case 'b': ch = '\b'; break;
+                case 'f': ch = '\f'; break;
+                case 't': ch = '\t'; break;
+                case 'n': ch = '\n'; break;
+                case 'r': ch = '\r'; break;
+                case '\\':
+                case '\'':
+                case '\"':
+                    break;
+                case 'u':
+                    if (i + 4 > end)
+                        return null;
+                    ch = 0;
+                    for (int j = 0; j < 4; j++) {
+                        int d = Character.digit(str.charAt(i++), 16);
+                        if (d < 0)
+                            return null;
+                        ch = (char) ((ch << 4) + d);
+                    }
+                }
+            }
+            buf.append(ch);
+        }
+        return buf.toString();
+    }
+}
