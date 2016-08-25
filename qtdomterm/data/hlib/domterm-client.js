363b356bdf576a4adde273c61bed6a609d81fa39
Per Bothner <per@bothner.com>
2016-08-24 17:25:41 -0700

    Add missing file qtdomterm/data/hlib/domterm-client.js. Update .gitignore.

---
+++ qtdomterm/data/hlib/domterm-client.js
@@ -0,0 +1,43 @@
+function loadHandler(event) {
+    var wt = new DomTerm("term1");
+    window.domterm1 = wt;
+    var topNode = document.getElementById("domterm");
+
+    var query = location.search;
+    var ws = query.match(/ws=([^&]*)/);
+    if (ws) {
+        var wsocket = new WebSocket(ws[1]);
+        wt.processInputCharacters = function(str) { wsocket.send(str); };
+        wsocket.onmessage = function(evt) {  wt.insertString(evt.data); }
+        var topNode = document.getElementById("term1");
+        wsocket.onopen = function(e) {
+            wsocket.send("\x92VERSION QtDomTerm;"+wt.versionInfo+"\n");
+            wt.initializeTerminal(topNode); };
+        return;
+    }
+    new QWebChannel(qt.webChannelTransport,
+        function(channel) {
+            var backend = channel.objects.backend;
+            window.backend = backend;
+            backend.reportEvent("VERSION", wt.versionInfo);
+            wt.initializeTerminal(topNode);
+            wt.processInputCharacters =
+                function(str) { if (backend) backend.processInputCharacters(str); };
+            wt.reportEvent =
+                function(name, data) { if (backend) backend.reportEvent(name, data); };
+	    wt.setWindowSize =
+                function(numRows, numColumns, availHeight, availWidth) {
+                    if (backend)
+                        backend.setWindowSize(numRows, numColumns,
+                                              availHeight, availWidth);
+            };
+            wt.log =
+                function(str) {
+                    if (backend) backend.log(str); };
+            wt.close = function() { if (backend) backend.close(); }
+            backend.write.connect(function (msg) {
+                wt.insertString(msg); });
+            backend.run();
+    });
+}
+window.addEventListener("load", loadHandler, false);
