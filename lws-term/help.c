f92d3e08ec01f3ba042045a7d2363db18677daa4
Per Bothner <per@bothner.com>
2018-01-08 12:49:05 -0800

    Merge '--help' and 'help domterm' texts.

--- lws-term/help.c
+++ lws-term/help.c
@@ -41,67 +41,6 @@ struct help_info help_table[] = {
   { NULL, NULL }
 };
 
-void print_help(FILE* out) {
-    fprintf(out,
-            "domterm is a terminal emulator/multipler that uses web technologies\n\n"
-            "USAGE:\n"
-            "    domterm [options] [<command> [<arguments...>]]\n\n"
-            "COMMANDS:\n"
-            "Sessions\n"
-            "  [window-specifer] [new] [program [args] - new terminal\n"
-            "  [window-specifer] [attach] session-specifier - (re-)attach session\n"
-            "  list - list active sessions\n"
-            "Miscellaneous subcommands:\n"
-            "  help [subcommand]\n"
-            "  [window-specifer] browse URL - open browser in new windowL\n"
-            "  is-domterm - succeeds if running under DomTerm\n"
-            "  browse url - view html or image in browser (sub-)window\n"
-            "  view-saved - view save-as-html file\n"
-            "Subcommands for output:"
-            "  html [or hcat] - display htmll]\n"
-            "  image [or imgcat] - display imagel\n"
-            "  fresh-line - if not at beginning of line, starts a fresh line\n"
-            "Subcommands for styling:\n"
-            "  reverse-video on|off - switch between dark or light background\n"
-            "  add-style - add a new rule to a temporary stylesheet\n"
-            "  load-stylesheet - load new stylesheet from a file\n"
-            "  list-stylesheets - list loaded stylesheets\n"
-            "  print-stylesheet - print stylesheet rules from a loaded stylesheet\n"
-            "  disable-stylesheet - disable a stylesheet\n"
-            "  enable-stylesheet - enable a stylesheet\n"
-
-            "OPTIONS:\n"
-            "    --browser[=command]     Create browser window for terminal.\n"
-            "                            The command can have a '%U' which is replaced by a URL; otherwise ' %U' is append to the command,\n"
-            "                            If no command specified, uses default browser\n"
-#if 0
-            "    --port, -p              Port to listen (default: '0' for random port)\n"
-            "    --interface, -i         Network interface to bind (eg: eth0), or UNIX domain socket path (eg: /var/run/ttyd.sock)\n"
-            "    --credential, -c        Credential for Basic Authentication (format: username:password)\n"
-            "    --uid, -u               User id to run with\n"
-            "    --gid, -g               Group id to run with\n"
-            "    --signal, -s            Signal to send to the command when exit it (default: SIGHUP)\n"
-            "    --reconnect, -r         Time to reconnect for the client in seconds (default: 10)\n"
-            "    --readonly, -R          Do not allow clients to write to the TTY\n"
-            "    --client-option, -t     Send option to client (format: key=value), repeat to add more options\n"
-            "    --check-origin, -O      Do not allow websocket connection from different origin\n"
-            "    --once, -o              Accept only one client and exit on disconnection\n"
-            "    --index, -I             Custom index.html path\n"
-            "    --ssl, -S               Enable SSL\n"
-            "    --ssl-cert, -C          SSL certificate file path\n"
-            "    --ssl-key, -K           SSL key file path\n"
-            "    --ssl-ca, -A            SSL CA file path for client certificate verification\n"
-            "    --debug, -d             Set log level (0-9, default: 0)\n"
-#endif
-            "    --version, -v           Print the version and exit\n"
-            "    --help, -h              Print this text and exit\n"
-            "If no --port option is specified, --browser is implied.\n"
-            "VERSION:\n"
-            "    %s\n\n",
-            LDOMTERM_VERSION
-    );
-}
-
 void print_help_file(const char* name, FILE *out)
 {
     char *hdir = get_bin_relative_path(DOMTERM_DIR_RELATIVE "/help");
@@ -114,7 +53,7 @@ void print_help_file(const char* name, FILE *out)
         copy_html_file(rfile, out);
         return;
     }
-    sprintf(buf, "%s/%s.rst", hdir, name);
+    sprintf(buf, "%s/%s.adoc", hdir, name);
     FILE *rfile = fopen(buf, "r");
     if (rfile == NULL)
         goto err;
@@ -123,6 +62,10 @@ void print_help_file(const char* name, FILE *out)
     return;
   err:
     fprintf(out, "cannot find help file %s\n", buf);
+}
+
+void print_help(FILE* out) {
+  print_help_file("domterm", out);
 }
 
 int help_action(int argc, char** argv, const char*cwd,
