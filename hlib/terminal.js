941f368d39286d85140a2cc770d4a447182e41f0
Per Bothner <per@bothner.com>
2019-07-01 14:23:59 -0700

    3 small fixes to TAB handling.

--- hlib/terminal.js
+++ hlib/terminal.js
@@ -1490,7 +1490,7 @@ Terminal.prototype.moveToAbs = function(goalAbsLine, goalColumn, addSpaceAsNeede
                     tcol = Number(st.substring(9));
                 }
                 if (! isNaN(tcol) && tcol > 0) {
-                    tcol = (column / tcol) * tcol + tcol;
+                    tcol = Math.trunc(column / tcol) * tcol + tcol;
                     if (goalColumn >= tcol) {
                         column = tcol;
                         handled = true;
@@ -1592,7 +1592,7 @@ Terminal.prototype.moveToAbs = function(goalAbsLine, goalColumn, addSpaceAsNeede
             // If there is a child, go the the first child next.
             var ch;
             if (current instanceof Node) {
-                if (current instanceof Element) {
+                if (current instanceof Element && !handled) {
                     var valueAttr = current.getAttribute("value");
                     if (this.isObjectElement(current))
                         column += 1;
@@ -4020,7 +4020,7 @@ Terminal.prototype.updateCursorCache = function() {
                             }
                         }
                         if (tcol)
-                            col = (col / tcol) * tcol + tcol;
+                            col = Math.trunc(col / tcol) * tcol + tcol;
                         else
                             col = this.nextTabCol(col);
                     } else if (ch == 10 || ch == 13 || ch == 12) {
