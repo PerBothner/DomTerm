60fc7741e4c37f4099a61c52aa5e7b95168113b6
Per Bothner <per@bothner.com>
2019-11-07 18:31:28 -0800

    changes to utils.py, escpecially new display_html function

--- tools/python/domterm/utils.py
+++ tools/python/domterm/utils.py
@@ -1,57 +1,21 @@
 import sys, traceback, base64, re
 from io import BytesIO
 
-def print_html(html):
-    sys.stdout.write("\x1b]72;" + html + "\x07")
+def print_html(html, out=sys.stdout):
+    out.write("\x1b]72;" + html + "\x07")
 
-def fac(x):
-    return x * x / x * fac(x - 1)
-def odd(x):
-    return x * x / x * even(x - 1)
-def even(x):
-    return x * x / x * odd(x - 1)
-    
-def display_data(content_type, content, overwrite=False, toggle=False, display="block", exit_page=False, stderr=False):
-    """Display data of specified content type. May be untrusted images.
-    toggle allows images to be hidden by clicking.
-    """
-
-    params = ""
-    #if overwrite:
-    #    params += " overwrite='yes'"
-    if toggle:
-        params += " toggle='yes'"
-    if exit_page:
-        params += " exit_page='yes'"
-
-    id = "python_image"
-    html = '<img %s src="data:%s;base64,%s"/>' % (params, content_type, base64.b64encode(content).decode())
-    out = sys.stderr if stderr else sys.stdout
+def display_html(html, overwrite=False, id='python_image', inline=False, out=sys.stdout):
+    top = 'span' if inline else 'div'
     if overwrite:
         out.write("\x1b]721;"+id+";"+html+"\x07")
     else:
-        out.write("\x1b]72;<div class='can-replace-children' replace-key='"+id+"' style='overflow-x: auto'>"+html+"</div>\x07")
+        out.write("\x1b]72;<"+top+" class='can-replace-children' replace-key='"+id+"' style='overflow-x: auto'>"+html+"</"+top+">\x07")
 
-class BlobBytesIO(BytesIO):
-    def __init__(self, max_bytes=25000000):
-        self.blob_max_bytes = max_bytes
-        BytesIO.__init__(self)
-
-    def write(self, s):
-        if self.tell()+len(s) > self.blob_max_bytes:
-            raise RuntimeError("Blob size exceeds limit of %s bytes" % self.blob_max_bytes)
-        BytesIO.write(self, s)
-
-    def close(self):
-        data = self.getvalue()
-        BytesIO.close(self)
-        return data
-
-#Saved_displayhook = sys.displayhook
+dt_display_hook = None
 def set_notebook_mode(enable=True):
     def auto_display(value):
-        if "display_hook" in globals():
-            expr = globals()["display_hook"](value)
+        if dt_display_hook:
+            expr = dt_display_hook(value)
         if hasattr(value, "_repr_html_"):
             print_html(value._repr_html_())
             return
