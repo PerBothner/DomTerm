d775f73781f8a003546c01fb8a6ef36835fdacb7
Per Bothner <per@bothner.com>
2020-06-20 11:49:39 -0700

    New file (belated)

---
+++ lws-term/option-names.h
@@ -0,0 +1,17 @@
+OPTION_S(default_frontend, "frontend.default")
+OPTION_S(shell_command, "shell.default")
+OPTION_S(command_firefox, "commmand.firefox")
+OPTION_S(command_chrome, "commmand.chrome")
+OPTION_S(command_electron, "commmand.electron")
+/*OPTION_S(window_geometry, "geometry")*/
+OPTION_S(geometry, "geometry")
+OPTION_S(openfile_application, "open.file.application")
+OPTION_S(openlink_application, "open.link.application")
+
+/* front-end options */
+OPTION_F(style_user, "style.user")
+OPTION_F(style_dark, "style.dark")
+OPTION_F(style_caret, "style.caret")
+OPTION_F(style_qt, "style.qt")
+OPTION_F(keymap_master, "keymap.master")
+OPTION_F(keymap_line_edit, "keymap.line-edit")
