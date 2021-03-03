c4531f75505b8eeb47adc0fbba423fbbb99e972b
Per Bothner <per@bothner.com>
2021-03-03 14:36:37 -0800

    New file for makeinfo-produced manual

---
+++ web/style/dt-manual.js
@@ -0,0 +1,19 @@
+// Hook used by info.js to set sidebar-header logo+text
+function sidebarLinkAppendContents(a, h1) {
+    a.innerHTML = "<div class='logo'><span>DomTerm terminal emulator</span><img src='images/domterm1.svg'/></div>"
+}
+
+function fixCategory() {
+    let categories = document.body.getElementsByClassName("category");
+    for (let i = categories.length; --i >= 0; ) {
+        let category = categories[i];
+        let text = category.innerHTML;
+        let m = text.match(/^(..*): *$/);
+        if (m) {
+            category.innerHTML = m[1];
+        }
+    }
+}
+INFO_CONFIG = {
+    on_iframe_load: fixCategory
+};
