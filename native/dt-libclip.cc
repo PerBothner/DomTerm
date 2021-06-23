11dbe5ca32c31f5c2c7e1676701577b034a45c68
Per Bothner <per@bothner.com>
2021-06-23 14:59:18 -0700

    Add dt-clip.cc - stub wrapper from libclipboard

---
+++ native/dt-libclip.cc
@@ -0,0 +1,18 @@
+#include <libclipboard.h>
+#include <string.h>
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+    clipboard_c* clipboard_manager = clipboard_new(NULL);
+    clipboard_mode cmode =
+        argv[1] && strcmp(argv[1], "--print-selection") == 0 ? LCB_PRIMARY
+        : LCB_CLIPBOARD;
+
+    int length;
+    char *clipText = clipboard_text_ex(clipboard_manager, &length, cmode);
+    if (clipText && fwrite(clipText, 1, length, stdout) == length)
+        return 0;
+    return -1;
+}
