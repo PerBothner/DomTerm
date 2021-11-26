3ca687f902ba68bda717fd58f8983ac4dd41ee88
Per Bothner <per@bothner.com>
2021-11-25 18:55:47 -0800

    Don't specify "> " as continuation prompt for bash or zsh.

--- tools/shell-integration.zsh
+++ tools/shell-integration.zsh
@@ -5,7 +5,7 @@ function __prompt_precmd() {
     then
       _PROMPT_SAVE_PS1="$PS1"
       _PROMPT_SAVE_PS2="$PS2"
-      PS1=$'%{\e]133;P;k=i\a%}'$PS1$'%{\e]133;B\a\e]122;> \a%}'
+      PS1=$'%{\e]133;P;k=i\a%}'$PS1$'%{\e]133;B\a%}'
       PS2=$'%{\e]133;P;k=s\a%}'$PS2$'%{\e]133;B\a%}'
     fi
     if test "$_prompt_executing" != ""
