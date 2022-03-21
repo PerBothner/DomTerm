ac32a50e90912a31af4cb2c96dcba023256b86d6
Per Bothner <per@bothner.com>
2022-03-20 19:15:29 -0700

    Fix handling of OPEN-WINDOW command

--- lws-term/protocol.cc
+++ lws-term/protocol.cc
@@ -1412,7 +1412,8 @@ reportEvent(const char *name, char *data, size_t dlen,
         }
         const char* url = !data[0] || (data[0] == '#' && g0 == data + 1) ? NULL
             : data;
-        display_session(options, NULL, url, unknown_window);
+        display_session(options, NULL, url,
+                        url ? unknown_window : dterminal_window);
         if (geom != NULL)
             free(geom);
     } else if (strcmp(name, "DETACH") == 0) {
